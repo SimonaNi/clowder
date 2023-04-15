3af1807a31a97a0c967c98bd1fc7b3a099f5e973
Sss <125960911+SimonaNi@users.noreply.github.com>
2023-04-15 08:00:13 +0000

    malko ot border-a ne napravih mn

--- borders.cpp
+++ borders.cpp
@@ -1,1 +1,11 @@
 #include "borders.h"
+
+void field(){
+system("cls");
+cout<<" ";
+
+//tyk da ima tialto na zmiata
+
+
+}
+
--- borders.h
+++ borders.h
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-const int Wide = 30; //??
-const int Tall = 30; //da sa ednakvi vse pak
+const int Wide = 40; //??
+const int Tall = 40; //da sa ednakvi vse pak
 
 int x, y, catX, catY;
 
@@ -12,5 +12,6 @@ void catSpawn(){     //tyka ima oshte neshta ama ne mi se zanimava rn
                     //dobaviane na koga spawn i score
 catX = rand() % Wide;
 catY = rand() % Tall;
-
+int minScore = 0;
 }
+
